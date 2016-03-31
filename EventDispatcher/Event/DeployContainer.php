4fc670a2e1421b0eade63298668dbc2270d58acb
methylbro <thomas@gasc.fr>
2016-03-31 14:34:29 +0200

    adding comments

--- EventDispatcher/Event/DeployContainer.php
+++ EventDispatcher/Event/DeployContainer.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+* This file is part of the Meup TagCommander Bundle.
+*
+* (c) 1001pharmacies <http://github.com/1001pharmacies/tagcommander-bundle>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
 namespace Meup\Bundle\TagcommanderBundle\EventDispatcher\Event;
 
 use Symfony\Component\EventDispatcher\Event;
--- EventDispatcher/Subscriber/CollectorSubscriber.php
+++ EventDispatcher/Subscriber/CollectorSubscriber.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+* This file is part of the Meup TagCommander Bundle.
+*
+* (c) 1001pharmacies <http://github.com/1001pharmacies/tagcommander-bundle>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
 namespace Meup\Bundle\TagcommanderBundle\EventDispatcher\Subscriber;
 
 use Meup\Bundle\TagcommanderBundle\DataCollector\DataLayerCollector;
@@ -7,15 +16,27 @@ use Meup\Bundle\TagcommanderBundle\EventDispatcher\Event\Track;
 use Meup\Bundle\TagcommanderBundle\EventDispatcher\Event\DeployContainer;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
+/**
+ *
+ */
 class CollectorSubscriber implements EventSubscriberInterface
 {
+    /**
+     * @var DataLayerCollector
+     */
     protected $collector;
 
+    /**
+     * @param DataLayerCollector $collector
+     */
     public function __construct(DataLayerCollector $collector)
     {
         $this->collector = $collector;
     }
 
+    /**
+     * @return array
+     */
     public static function getSubscribedEvents()
     {
         return array(
@@ -24,6 +45,10 @@ class CollectorSubscriber implements EventSubscriberInterface
         );
     }
 
+    /**
+     * @param DeployContainer $event
+     * @return void
+     */
     public function onTcContainer(DeployContainer $event)
     {
         $this->collector->collectContainer(
@@ -34,6 +59,10 @@ class CollectorSubscriber implements EventSubscriberInterface
         );
     }
 
+    /**
+     * @param Track $event
+     * @return void
+     */
     public function onTcEvent(Track $event)
     {
         $this->collector->collectEvent(
